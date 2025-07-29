825d32fdb66db14f5065a23ca5e2e1b5fc171d85
anurag dey <05powel@gmail.com>
2025-07-29 17:48:31 +0530

    Initial commit with Node app and Jenkins setup

---
+++ app/server.js
@@ -0,0 +1,12 @@
+// server.js
+const http = require("http");
+const port = 3000;
+
+const requestHandler = (req, res) => {
+  res.end("Hello from Anurag's app running in Docker!");
+};
+
+const server = http.createServer(requestHandler);
+server.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
